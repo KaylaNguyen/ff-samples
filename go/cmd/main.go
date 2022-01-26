796ca85481e75d1e0e3ec8950af9dad583c0a38d
Kayla Nguyen <kaylanguyen@google.com>
2022-01-25 22:47:14 -0800

    http funcs throws exceptions + README

---
+++ go/cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"example.com/hello"
+	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
+)
+func main() {
+	ctx := context.Background()
+	if err := funcframework.RegisterHTTPFunctionContext(ctx, "/", hello.HelloWorld); err != nil {
+		log.Fatalf("funcframework.RegisterHTTPFunctionContext: %v\n", err)
+	}
+	// Use PORT environment variable, or default to 8080.
+	port := "8080"
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		port = envPort
+	}
+	if err := funcframework.Start(port); err != nil {
+		log.Fatalf("funcframework.Start: %v\n", err)
+	}
+}
---
+++ php/index.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright 2019 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * To use this, set the following environment variables:
+ *   FUNCTION_TARGET=helloHttp
+ *   FUNCTION_EVENT_TYPE=http
+ */
+
+use Psr\Http\Message\ServerRequestInterface;
+
+function helloHttp(ServerRequestInterface $request)
+{
+//     return sprintf(
+//         "Hello %s from PHP HTTP function!" . PHP_EOL,
+//         $request->getQueryParams()['name'] ?? 'World'
+//     );
+       throw new Exception("Intended exception");
+}
+
+/**
+ * To use this, set the following environment variables:
+ *   FUNCTION_TARGET=helloCloudEvent
+ *   FUNCTION_EVENT_TYPE=cloudevent
+ */
+
+use Google\CloudFunctions\CloudEvent;
+
+function helloCloudEvent(CloudEvent $cloudevent)
+{
+    $stdout = fopen('php://stdout', 'wb');
+    fwrite($stdout, $cloudevent);
+}
